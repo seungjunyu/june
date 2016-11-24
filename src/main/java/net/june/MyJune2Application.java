79fcbec418c912db9fd6ebb470d1031c278ce855
seungjunyu <seungjunyu@naver.com>
2016-11-24 17:45:56 +0900

    3번째 커밋

---
+++ src/main/java/net/june/MyJune2Application.java
@@ -0,0 +1,12 @@
+package net.june;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJune2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJune2Application.class, args);
+	}
+}
